d4bcc4663ec95e67e00f40a64d2f6527e4966ed7
Masaki Takahashi <takaha18@msu.edu>
2018-12-17 11:24:29 -0500

    Add files via upload
    4.12 (Date Class) Create a class called Date that includes three pieces of information as auto-implemented properties—a month (type int), a day (type int) and a year (type int). Your class should have a constructor that initializes the three automatic properties and assumes that the values provided are correct. Provide a method DisplayDate that displays the month, day and year separated by forward slashes (/). Write a test app named DateTest that demonstrates class Date’s capabilities.

---
+++ Chatper4Exercise12/Chatper4Exercise12/Date.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chatper4Exercise12
+{
+    
+    //Create a class called Date that includes three pieces of information
+    //as auto-implemented properties
+    class Date
+    {
+        public Date() : this(10, 28, 2018) { }
+        //a month (type int), a day (type int) and a year (type int). 
+        //Your class should have a constructor that initializes the three automatic
+        // properties and assumes that the values provided are correct.
+        public Date(int month, int day, int year)
+        {
+            Month = month;
+            Day = day;
+            Year = year;
+        }
+
+        public int Month { get; set; }
+        public int Day { get; set; }
+        public int Year { get; set; }
+        //Provide a method DisplayDate that displays the month, day and year
+        public string DisplayDate()
+        {    //separated by forward slashes (/)
+            return (Month + "/" + Day + "/" + Year);
+        }
+    }
+}
